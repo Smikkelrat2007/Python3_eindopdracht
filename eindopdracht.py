7a9f388e948c1834fa1f633e7dd5a4d6e34f8992
OnezSadim <zenofrederik@gmail.com>
2022-05-31 11:02:45 +0200

    de veranderingen die Monique wou aanbrengen

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,35 +1,24 @@
-nummers = {"albert" : "1" , "berta" : "2", "clara" : "3"}
-global key_list
-global val_list
-key_list = list(nummers.keys())
-val_list = list(nummers.values())
-
-
-def print_lijst():
-  for key, value in nummers.items():
+def print_lijst(nummersl):
+  for key, value in nummersl.items():
     print(key,"-",value)
 
-def nieuw_contact():
+def nieuw_contact(nummers):
   nieuwecontactnaam = input('wat is de naam van je nieuwe contact?')
   nieuwenummervancontact = input('wat is het nummer van je nieuwe contact?')
   nummers[nieuwecontactnaam] = nieuwenummervancontact
 
-def verwijder_contact():
-  key_list = list(nummers.keys())
-  val_list = list(nummers.values())
+def verwijder_contact(nummers):
   verwijderr = input('wil je het contact verwijderen met behulp van de naam van het contact of met het nummer van het contact??? \n1. met het nummer \n2. met de naam')
   if verwijderr == "2":
     keyver = input('wat is de naam die je wilt verwijderen?')
     del nummers[keyver]
   if verwijderr == "1":
     valver = input('wat is het nummer die je wilt verwijderen?')
-    position = val_list.index(valver)
-    keyver = key_list[position]
-    del nummers[keyver]
+    for key, value in nummers.items():
+      if valver == value:
+        del nummers[key]
 
-def bewerk_contact():
-  key_list = list(nummers.keys())
-  val_list = list(nummers.values())
+def bewerk_contact(nummers):
   connabew = input('wil je het contact bewerken met behulp van de naam van het contact of met het nummer van het contact??? \n1. met de naam \n2. met het nummer')
   if connabew == "1":
     keybew = input('wat is de naam die je wilt bewerken?')
@@ -45,8 +34,9 @@ def bewerk_contact():
       nummers[keybew] = nieuwnummer
   if connabew == "2":
     valbew = input('wat is het nummer die je wilt bewerken?')
-    position = val_list.index(valbew)
-    keybew = key_list[position]
+    for key, value in nummers.items():
+      if valbew == value:
+        keybew = key
     naambewkeybew = input('wil je de naam bewerken of wil je het nummer bewerken? \n1. naam \n2. nummer')
     if naambewkeybew == "1":
       nieuwenaam = input('wat word de nieuwe naam?')
@@ -57,24 +47,26 @@ def bewerk_contact():
       del nummers[keybew]
       nummers[keybew] = nieuwnummer
 
+def main():
+  nummers = {"albert" : "1" , "berta" : "2", "clara" : "3"}
+  homevraag = 0
+  while homevraag != '5':
+    homevraag = input('wil je: \n1. print lijst uit \n2. maak een nieuw contact aan \n3. verwijder een contact \n4. bewerk een contact \n5. stop het progamma  >>>')
+    print(homevraag)
+    if homevraag == "1": #1
+      print_lijst(nummers)
 
-doorgaan = True
-while doorgaan:
-  homevraag = input('wil je: \n1. print lijst uit \n2. maak een nieuw contact aan \n3. verwijder een contact \n4. bewerk een contact \n5. stop het progamma')
+    elif homevraag == "2": #2
+      nieuw_contact(nummers)
 
-  if homevraag == "1": #1
-    print_lijst()
+    elif homevraag == "3": #3
+      verwijder_contact(nummers)
 
-  elif homevraag == "2": #2
-    nieuw_contact()
+    elif homevraag == "4": #4
+      bewerk_contact(nummers)
 
-  elif homevraag == "3": #3
-    verwijder_contact()
+    # stop optie toevoegen
 
-  elif homevraag == "4": #4
-    bewerk_contact()
+  print('doei!! :)')
 
-  elif homevraag == "5":
-    doorgaan = False
-
-print('doei!! :)')
+main()
